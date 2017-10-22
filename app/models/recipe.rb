5e820c9d15a136195d8e7cc33f1bdcd76e54a7ce
Ahmed Tabana <ahmedtabana10@gmail.com>
2017-10-22 19:51:13 -0400

    changing key

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe
   include HTTParty
 
   base_uri "http://food2fork.com/api"
-  default_params key: ENV["FOOD2FORK_KEY"]
+  default_params key: 'c81c936c62d87b9a977fd36c581ed109'
   format :json
 
   def self.for (keyword)
